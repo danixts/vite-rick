ab75243890b976558327a5ef4aa984e0f2d059ea
Daniel Miranda <dante789ki@gmail.com>
2021-09-19 03:28:38 -0400

    [ADD] filter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script setup>
 import Card from "./components/Card.vue";
-import { onMounted, ref } from "@vue/runtime-core";
+import { onMounted, ref, computed, reactive } from "vue";
 import axios from "axios";
 
-const results = ref({});
+const results = reactive([]);
 const count = ref(1);
-
+const search = ref("");
+const limit = ref(3)
 const handleNext = async () => {
-  count.value++;
+  if (count.value <= limit.value) count.value++
   await getData();
 };
 
@@ -16,13 +17,12 @@ const handlePrev = async () => {
   await getData();
 };
 
-const handleButton = async (e) => {
-  const data = results.value.filter((el) => el.name.toLowerCase().includes(e.target.value));
-  results.value = data.length > 0 ? data : results.value;
-};
+const querySearch = computed(() => results.filter(({ name }) => name.toLowerCase().includes(search.value)));
+
 const getData = async () => {
   const { data } = await axios.get(`https://rickandmortyapi.com/api/character/?page=${count.value}`);
-  results.value = data.results;
+  // results.splice(0, results.length);
+  if (count.value <= limit.value) results.push(...data.results);
 };
 
 onMounted(async () => {
@@ -31,27 +31,39 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="fixed top-0 w-full z-40 bg-primary-900/50 backdrop-blur-md shadow-lg">
+  <div class="fixed top-0 py-4 w-full z-40 bg-primary-900/50 backdrop-blur-md shadow-lg">
     <div class="container">
       <div class="flex flex-row justify-between items-center">
-        <button @click="handlePrev" class="btn">PREV</button>
-        <h4 class="text-white font-bold">Page {{ count }}</h4>
-        <input type="text" v-model="count" @input="handleButton" />
-        <button @click="handleButton" class="btn">IR</button>
-        <button @click="handleNext" class="btn">NEXT</button>
+        <div class="flex items-center space-x-4">
+          <input
+            type="text"
+            v-model="search"
+            placeholder="Buscar..."
+            class="bg-gray-100 rounded-md px-4 py-3 focus:ring-2 ring-yellow-400 focus:outline-none"
+          />
+          <!-- <button @click="handleButton" class="btn">IR</button> -->
+        </div>
+        <div class="flex flex-row space-x-4 items-center">
+          <!-- <button @click="handlePrev" class="btn">PREV</button> -->
+          <button @click="handleNext" class="btn">NEXT</button>
+          <h4 class="text-white font-bold">Page {{ count }}</h4>
+        </div>
       </div>
     </div>
   </div>
-  <h1 class="font-bold text-5xl text-center text-white pt-6">Rick and Morty</h1>
+  <h1 class="font-bold text-5xl text-center text-white pt-[120px]">Rick and Morty</h1>
   <div class="container my-8">
     <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-      <Card :data="data" v-for="data in results" />
+      <Card :data="data" v-for="(data, index) in querySearch" :key="index" />
     </div>
+    <template v-if="querySearch.length === 0">
+      <h1 class="text-center text-white/25 mt-32 w-full text-3xl">Datos no encontrados</h1>
+    </template>
   </div>
 </template>
 
 <style>
 .btn {
-  @apply bg-yellow-400 px-4 py-3 font-bold text-white;
+  @apply bg-yellow-400 px-4 py-3 font-bold text-white transition-all hover:bg-yellow-500 duration-500 rounded-md;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,38 +1,42 @@
 <script setup>
+import { ref } from "@vue/reactivity";
+
 defineProps({
   data: Object,
 });
+
+const load = ref(false);
+
+const lazy = () => {
+  load.value = true;
+};
 </script>
 
 <template>
   <div
-    class="
-      group
-      flex flex-row
-      justify-start
-      items-center
-      bg-gradient-to-br
-      from-primary-700
-      to-primary-600
-      rounded-md
-      hover:bg-primary-600/80
-      transition-all
-      cursor-pointer
-      hover:scale-105 hover:shadow-2xl
-      shadow-xl
-      relative
-    "
+    class="group flex flex-row justify-start items-center bg-gradient-to-br from-primary-700 to-primary-600 rounded-md hover:bg-primary-600/80 transition-all cursor-pointer hover:scale-105 hover:shadow-2xl shadow-xl relative"
   >
     <span class="absolute top-3 right-3 text-white/25 text-xs">{{ data.origin.name }}</span>
-    <img :src="data.image" alt="" class="object-cover w-[180px] rounded-l-md" />
+    <div
+      v-if="!load"
+      class="object-cover min-w-[180px] min-h-[180px] w-[180px] rounded-l-md bg-primary-900"
+    ></div>
+    <img
+      :src="data.image"
+      alt
+      class="object-cover min-w-[180px] min-h-[180px] w-[180px] rounded-l-md transition-all duration-500"
+      :class="!load ? 'opacity-0' : 'opacity-100'"
+      v-show="load"
+      @load="lazy"
+    />
     <div class="px-8">
-      <h3 class="text-white font-bold text-center text-2xl group-hover:text-yellow-300 transition-colors">{{
-        data.name
-      }}</h3>
+      <h3
+        class="text-white font-bold text-center text-2xl group-hover:text-yellow-300 transition-colors"
+      >{{ data.name }}</h3>
       <div class="flex flex-row space-x-2 items-center mt-2">
         <div
-          class="rounded-full w-[8px] h-[8px]"
-          :class="data.status === 'Alive' ? 'bg-green-500' : 'bg-red-500'"
+          class="shadow-lg rounded-full w-[10px] h-[10px]"
+          :class="data.status === 'Alive' ? 'bg-gradient-to-br from-green-500 to-green-700' : 'bg-gradient-to-br from-red-500 to-red-700'"
         ></div>
         <span class="text-white/60 uppercase">{{ data.status }} - {{ data.species }}</span>
       </div>
